35d301eaf0d6abb45d1330e6dcb2205be94cb5b3
Pratik Chaudhari <pratikc1020@gmail.com>
2021-07-23 15:23:31 +0530

    Added ability to demonstrate file operations

---
+++ src/main/java/com/employeepayroll/EmployeePayRollData.java
@@ -0,0 +1,20 @@
+package com.employeepayroll;
+
+public class EmployeePayRollData {
+    //Variables
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayRollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    //Printing the Result OverRide
+    @Override
+    public String toString () {
+        return "Id = " +id+ "\n Name = " +name+ "\n salary = " +salary;
+    }
+}
